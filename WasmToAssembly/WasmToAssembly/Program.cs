b9a02e8f0cc4d0a55a2358da2aa730662e87b14a
Jason Bock <jason@jasonbock.net>
2017-08-15 08:10:01 -0700

    Added an example for using WebAssembly Nuget package
    Sadly doesn't work yet, but added this issue to track it:
    https://github.com/RyanLamansky/dotnet-webassembly/issues/4

---
+++ WasmToAssembly/WasmToAssembly/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WebAssembly;
+
+namespace WasmToAssembly
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var module = Compile.FromBinary<dynamic>("collatz.wasm");
+			Console.WriteLine(module().Exports.collatz(5));
+		}
+	}
+}
