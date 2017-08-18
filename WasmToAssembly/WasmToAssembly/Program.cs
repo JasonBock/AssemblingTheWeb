127e98d89c8831c8b0eee37ce8ef849fc458dee8
Jason Bock <jason@jasonbock.net>
2017-08-18 08:57:20 -0500

    Updated for working WebAssembly NuGet package

--- WasmToAssembly/WasmToAssembly/Program.cs
+++ WasmToAssembly/WasmToAssembly/Program.cs
@@ -8,7 +8,15 @@ namespace WasmToAssembly
 		static void Main(string[] args)
 		{
 			var module = Compile.FromBinary<dynamic>("collatz.wasm");
-			Console.WriteLine(module().Exports.collatz(5));
+			// 5 => 16, 8, 4, 2, 1 == 5
+			// 10 => 5, 16, 8, 4, 2, 1 == 6
+			// 11 => 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 == 14
+			Console.WriteLine(module().Exports.collatz(11));
 		}
+	}
+
+	public interface ICollatz
+	{
+		int collatz(int start);
 	}
 }
