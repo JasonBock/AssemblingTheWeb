b80e4460aca84cadc73eef2271fef1a4d1b64b52
Jason Bock <jason@jasonbock.net>
2018-08-14 21:36:28 -0700

    Adding Blaatz

---
+++ Blaatz/Blaatz.Core/CollatzSequence.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Blaatz.Core
+{
+	public static class CollatzSequence
+	{
+		public static List<BigInteger> GetSequence(BigInteger number)
+		{
+			var sequence = new List<BigInteger>() { number };
+
+			while (number > 1)
+			{
+				number = number % 2 == 0 ?
+					number / 2 : ((3 * number) + 1) / 2;
+				sequence.Add(number);
+			}
+
+			return sequence;
+		}
+	}
+}
